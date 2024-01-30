87c9c8ba7b90e9ca5f187fe6577913039b05a7ef
Leonard <Leonardhernandez02@gmail.com>
2024-01-30 00:02:56 -0500

    Hello word

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("Hola mundo desde java");
+        String nombre = "Leonard";
+
+        System.out.println("Hola " + nombre);
+    }
+}
