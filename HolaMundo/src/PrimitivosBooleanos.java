5d8097d31dbc3e0a268097d3e1edd6363921b7ae
Leonard <Leonardhernandez02@gmail.com>
2024-02-03 22:36:09 -0500

    booleanos

---
+++ HolaMundo/src/PrimitivosBooleanos.java
@@ -0,0 +1,19 @@
+public class PrimitivosBooleanos {
+
+    public static void main(String[] args) {
+
+        boolean datoLogico = true;
+        System.out.println("datoLogico = " + datoLogico);
+
+        double d= 64645.433-3;
+        float f = 1.4565e2f;
+
+        datoLogico = (d>f);
+        System.out.println(datoLogico);
+
+        boolean esIgual = (3-2 == 1);
+        System.out.println(esIgual);
+
+    }
+
+}
