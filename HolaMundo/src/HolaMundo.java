712083e2b5fd5449aa25dee497b39e7049ed44c9
Leonard <Leonardhernandez02@gmail.com>
2024-02-03 21:40:53 -0500

    variables

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,8 +1,44 @@
 public class HolaMundo {
     public static void main(String[] args){
-        System.out.println("Hola mundo desde java");
+        String saludar = "Hola mundo desde java";
+        System.out.println(saludar);
         String nombre = "Leonard";
 
+        System.out.println(saludar.toUpperCase());
         System.out.println("Hola me llamo," + nombre);
+
+        Integer numero = 10;
+        System.out.println(numero);
+
+        boolean valor = true;
+        int numero2 = 0;
+        if(valor){
+            System.out.println(numero);
+            numero2 = 10;
+        }
+        System.out.println(numero2);
+
+        var numero3 = 15;
+
+        String nombre1;
+
+        nombre = "andres";
+
+        if(numero > 10){
+            nombre = "juan";
+        }
+
+        System.out.println(nombre);
+
+        int edadPersona = 5;
+
+        //comentar una linea
+
+        /*
+         comentar bloque
+         hola
+        */
+
+
     }
 }
