76bccad9f29124f4cadb93e0643ba882f8a3266f
Leonard <Leonardhernandez02@gmail.com>
2024-02-04 22:57:40 -0500

    joption panel y refactor sistemas numericos

--- HolaMundo/src/SistemasNumericos.java
+++ HolaMundo/src/SistemasNumericos.java
@@ -1,25 +1,42 @@
+import javax.swing.*;
+
 public class SistemasNumericos {
 
     public static void main(String[] args) {
+        int numeroDecimal = 0;
 
-        int numeroDecimal = 500;
+        try{
+            numeroDecimal = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese numero"));
+        }catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null,"Solo se aceptan numeros");
+            main(args);
+            System.exit(0);
+        }
+
         System.out.println("numeroDecimal = " + numeroDecimal);
 
-        System.out.println("numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+        String mensajeBinario ="numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(mensajeBinario);
 
         int numeroBinario = 0b111110100;
-        System.out.println("numeroBinario = " + numeroBinario);
+        System.out.println("numeroBinario = " + numeroDecimal);
 
-        System.out.println("numero octal de " + numeroDecimal+ " = " + Integer.toOctalString(numeroDecimal));
+        String mensajeOctal = "numero octal de " + numeroDecimal+ " = " + Integer.toOctalString(numeroDecimal);
+        System.out.println(mensajeOctal);
+
         int numeroOctal = 0764;
         System.out.println("numeroOctal = " + numeroOctal);
 
-        System.out.println("numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal) );
+        String mensajeHex = "numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal) ;
+        System.out.println( mensajeHex);
         int numero = 0x1f4;
+
         System.out.println("numero = " + numero);
 
-
-
+        String mensaje = mensajeBinario;
+        mensaje += System.lineSeparator() + mensajeOctal;
+        mensaje += System.lineSeparator() + mensajeHex;
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 
 }
