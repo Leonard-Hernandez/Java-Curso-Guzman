aad4c9c8ea76e95c7e9b4c9ec6341f407fec071f
Leonard <Leonardhernandez02@gmail.com>
2024-02-18 22:24:02 -0500

    Ejecicio dia mes

--- HolaMundo/src/flujoDeControl/SentenciaSwitchCase.java
+++ HolaMundo/src/flujoDeControl/SentenciaSwitchCase.java
@@ -1,7 +1,56 @@
 package flujoDeControl;
 
+import javax.swing.*;
+
 public class SentenciaSwitchCase {
     public static void main(String[] args) {
+
+        int mes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero del mes"));
+        String nombreMes = null;
+
+        switch (mes){
+            case 1:
+                nombreMes = "Enero";
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                break;
+            case 4:
+                nombreMes = "Abril";
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                break;
+            case 6:
+                nombreMes = "Junio";
+                break;
+            case 7:
+                nombreMes = "Julio";
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                break;
+            case 9:
+                nombreMes = "Septiembre";
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                break;
+            default:
+                nombreMes = "No se encontro el mes";
+        }
+
+        System.out.println("El nombre del mes es ".concat(nombreMes));
+
 
         char num = '0';
 
