68e6e5df401796e94495647e3f12dc311a6b1ee4
Leonard <Leonardhernandez02@gmail.com>
2024-02-18 22:24:27 -0500

    Renombre

--- HolaMundo/src/flujoDeControl/SentenciaIfElseNumeroDiasMes.java
+++ HolaMundo/src/flujoDeControl/SentenciaIfElseNumeroDiasMes.java
@@ -2,7 +2,7 @@ package flujoDeControl;
 
 import java.util.Scanner;
 
-public class NumeroDiasMes {
+public class SentenciaIfElseNumeroDiasMes {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
