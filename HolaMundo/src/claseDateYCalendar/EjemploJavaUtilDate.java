3c09b768368903aeca57472e5591afdd22628003
Leonard <Leonardhernandez02@gmail.com>
2024-02-25 20:11:51 -0500

    Usos basicos java date

---
+++ HolaMundo/src/claseDateYCalendar/EjemploJavaUtilDate.java
@@ -0,0 +1,34 @@
+package claseDateYCalendar;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EjemploJavaUtilDate {
+    public static void main(String[] args) {
+
+        Date fecha = new Date();
+
+        System.out.println("fecja = " + fecha);
+
+        SimpleDateFormat df = new SimpleDateFormat("dd MMMM, yyyy");
+        String fechaStr = df.format(fecha);
+
+        long j = 0;
+        for(int i = 0; i<100000000;i++){
+            j +=i;
+
+        }
+        System.out.println("j = " + j);
+
+        Date fecha2 = new Date();
+        long tiempoFinal = fecha2.getTime() - fecha.getTime();
+        System.out.println("tiempo transcurrido en el for = " + tiempoFinal);
+
+        System.out.println("fechaStr = " + fechaStr);
+
+
+
+
+
+    }
+}
