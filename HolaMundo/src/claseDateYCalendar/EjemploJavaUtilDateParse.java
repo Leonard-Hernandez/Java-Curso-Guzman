c7afdd0e0ddfe83f66e1c9c264d3dded856527d5
Leonard <Leonardhernandez02@gmail.com>
2024-02-26 21:16:37 -0500

    Comparar fechas

---
+++ HolaMundo/src/claseDateYCalendar/EjemploJavaUtilDateParse.java
@@ -0,0 +1,46 @@
+package claseDateYCalendar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class EjemploJavaUtilDateParse {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            System.out.println("Ingrese una feucha con formato 'yyyy-MM-dd'");
+            Date fecha = format.parse(scanner.next());
+            System.out.println("fecha = " + fecha);
+            System.out.println("format = " + format.format(fecha));
+            
+            Date fecha2 = new Date();
+            
+            if(fecha.after(fecha2)){
+                System.out.println("Fecha1 es despues o mayor a fecha2");
+            } else if (fecha.before(fecha2)) {
+                System.out.println("Fecha1 es anterior fecha2");
+            }else if(fecha.equals(fecha2)){
+                System.out.println("fechas Iguales");
+            }
+
+            if(fecha.compareTo(fecha2)>0){
+                System.out.println("Fecha1 es despues o mayor a fecha2");
+            } else if (fecha.compareTo(fecha2)<0) {
+                System.out.println("Fecha1 es anterior fecha2");
+            } else if (fecha.compareTo(fecha2) == 0) {
+                System.out.println("fechas iguales");
+            }
+
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        
+        
+    }
+}
