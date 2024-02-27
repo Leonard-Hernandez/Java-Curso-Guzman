b688ce5a9d2ca3cf46ad247d85a48bff2a439cc2
Leonard <Leonardhernandez02@gmail.com>
2024-02-26 21:43:11 -0500

    Viendo las propiedades del sistema con la clase System

---
+++ HolaMundo/src/claseSystem/EjemploPropiedadesDeSistema.java
@@ -0,0 +1,26 @@
+package claseSystem;
+
+import java.util.Properties;
+
+public class EjemploPropiedadesDeSistema {
+    public static void main(String[] args) {
+
+        String username = System.getProperty("user.name");
+        System.out.println("username = " + username);
+
+        String home = System.getProperty("user.home");
+        System.out.println("home = " + home);
+
+        String workspace = System.getProperty("user.dir");
+        System.out.println("workspace = " + workspace);
+
+        String java = System.getProperty("java.version");
+        System.out.println("java = " + java);
+
+        String lineseparation = System.getProperty("line.separator");
+        System.out.println("lineseparation = " + lineseparation + "salto de linea");
+
+        Properties p = System.getProperties();
+        p.list(System.out);
+    }
+}
