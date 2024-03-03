b3c7410665106618ced606f62aef9a3a119db3d4
Leonard <Leonardhernandez02@gmail.com>
2024-03-02 22:02:25 -0500

    Usando el garbaje colector explicitamente

---
+++ HolaMundo/src/claseMath/EjemploClaseMath.java
@@ -0,0 +1,26 @@
+package claseMath;
+
+public class EjemploClaseMath {
+    public static void main(String[] args) {
+        int absoluta = Math.abs(-3);
+        System.out.println("absoluta = " + absoluta);
+
+        absoluta = Math.abs(3);
+        System.out.println("absoluta = " + absoluta);
+
+        double max = Math.max(3.5,2.2);
+        System.out.println("max = " + max);
+
+        double min = Math.min(3.5,2.2);
+        System.out.println("min = " + min);
+
+        double techo = Math.ceil(3.5);
+        System.out.println("techo = " + techo);
+
+        double piso = Math.floor(3.5);
+        System.out.println("piso = " + piso);
+
+        long entero = Math.round(3.5);
+        System.out.println("entero = " + entero);
+    }
+}
