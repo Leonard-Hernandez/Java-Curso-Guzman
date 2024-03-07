3ad30e45431a924b62fa0116a57d37944250338a
Leonard <Leonardhernandez02@gmail.com>
2024-03-06 21:50:08 -0500

    terminando la clase ejemplo arreglos

--- HolaMundo/src/arreglos/EjemploArreglos.java
+++ HolaMundo/src/arreglos/EjemploArreglos.java
@@ -3,12 +3,16 @@ public class EjemploArreglos {
 
         int[] numeros = new int[4];
 
-        numeros[0] = 0;
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
 
         int i = numeros[0];
         int j = numeros[1];
         int k = numeros[2];
-        int l = numeros[3];
+        //obtener el ultimo
+        int l = numeros[numeros.length-1];
 
         System.out.println("i = " + i);
         System.out.println("j = " + j);
