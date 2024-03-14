5cf150a3bda1f5ff6c994fb9dd162eda3b800aed
Leonard <Leonardhernandez02@gmail.com>
2024-03-13 21:15:35 -0500

    Optimizando ordenamiento Burbuja

--- HolaMundo/src/arreglos/EjemploArreglosForOrdenamientoBurbuja.java
+++ HolaMundo/src/arreglos/EjemploArreglosForOrdenamientoBurbuja.java
@@ -18,11 +18,12 @@ public class EjemploArreglosForOrdenamientoBurbuja {
         int total = productos.length;
 
         for(int i = 0; i<total;i++){
-            for (int j = 0; j< total; j++){
-                if(productos[i].compareTo(productos[j]) < 0){
-                    String auxiliar = productos[i];
-                    productos[i] = productos[j];
-                    productos[j] = auxiliar;
+
+            for (int j = 0; j< total-1 -i; j++){
+                if(productos[j+1].compareTo(productos[j]) < 0){
+                    String auxiliar = productos[j];
+                    productos[j] = productos[j+1];
+                    productos[j+1] = auxiliar;
                 }
             }
         }
