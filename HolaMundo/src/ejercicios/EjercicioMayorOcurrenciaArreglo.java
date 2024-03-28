5f76752ccdece2f52e841173e5ec3b8b4f357c24
Leonard <Leonardhernandez02@gmail.com>
2024-03-27 21:12:42 -0500

    Mayor concurrencia de un arreglo

---
+++ HolaMundo/src/ejercicios/EjercicioMayorOcurrenciaArreglo.java
@@ -0,0 +1,39 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class EjercicioMayorOcurrenciaArreglo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arreglo = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Ingrese un numero de 1 a 9: ");
+            arreglo[i] = scanner.nextInt();
+        }
+
+        int[] arregloCantidadValores = new int[10];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            int cantidad = 0;
+            for (int j = 0; j < arreglo.length; j++) {
+                if (arreglo[i] == arreglo[j]) {
+                    cantidad++;
+                }
+            }
+            arregloCantidadValores[i] = cantidad;
+        }
+
+        int indice = 0;
+        int max = 0;
+        for (int i = 0; i < 10; i++) {
+            if (max < arregloCantidadValores[i]) {
+                max = arregloCantidadValores[i];
+                indice = i;
+            }
+        }
+        System.out.println("La mayor ocurrencia es: " + max);
+        System.out.println("El elemento que mas se repite es: " + arreglo[indice]);
+        System.out.println("el elemento " + arreglo[indice] + " esta repetido " + max + "veces!");
+    }
+}
