49fb398e979940ca91cc3ecce0d96c3889d6164d
Leonard <Leonardhernandez02@gmail.com>
2024-05-21 21:35:41 -0500

    dando formato

--- HolaMundo/src/pooHerencia/Alumno.java
+++ HolaMundo/src/pooHerencia/Alumno.java
@@ -6,32 +6,37 @@ public class Alumno extends Persona {
     private double notaMatematicas;
     private double notaLenguaje;
     private double notaHistoria;
-    
+
     public String getInstitucion() {
         return institucion;
     }
+
     public void setInstitucion(String institucion) {
         this.institucion = institucion;
     }
+
     public double getNotaMatematicas() {
         return notaMatematicas;
     }
+
     public void setNotaMatematicas(double notaMatematicas) {
         this.notaMatematicas = notaMatematicas;
     }
+
     public double getNotaLenguaje() {
         return notaLenguaje;
     }
+
     public void setNotaLenguaje(double notaLenguaje) {
         this.notaLenguaje = notaLenguaje;
     }
+
     public double getNotaHistoria() {
         return notaHistoria;
     }
+
     public void setNotaHistoria(double notaHistoria) {
         this.notaHistoria = notaHistoria;
     }
-
-    
 
 }
