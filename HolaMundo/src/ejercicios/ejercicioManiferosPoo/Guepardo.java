e3e72f391f3956d7050343a348f257a7aa6403f5
Leonard <Leonardhernandez02@gmail.com>
2024-06-06 20:20:00 -0500

    terminando creacion de los felinos

--- HolaMundo/src/ejercicios/ejercicioManiferosPoo/Guepardo.java
+++ HolaMundo/src/ejercicios/ejercicioManiferosPoo/Guepardo.java
@@ -1,5 +1,35 @@
 package ejercicios.ejercicioManiferosPoo;
 
-public class Guepardo {
+import ejercicios.ejercicioManiferosPoo.tiposManiferos.Felino;
+
+public class Guepardo extends Felino{
+
+    public Guepardo() {}
+
+    public Guepardo(String habitat, Float altura, Float largo, Float peso, Float nombreCientifico, Float tamanoGarras, int velocidad) {
+        super(habitat, altura, largo, peso, nombreCientifico, tamanoGarras, velocidad);
+    }
+
+    @Override
+    public String comer() {
+        return "El guepardo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " come ratones con sus garras de " + getTamanoGarras();
+    }
+
+    @Override
+    public String dormir() {
+        return "El guepardo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " duerme";
+    }
+
+    @Override
+    public String correr() {
+        return "El guepardo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " corre";
+    }
+
+    @Override
+    public String comunicarse() {
+        return "El guepardo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " comunica";
+    }
+
+
 
 }
--- HolaMundo/src/ejercicios/ejercicioManiferosPoo/Leon.java
+++ HolaMundo/src/ejercicios/ejercicioManiferosPoo/Leon.java
@@ -1,5 +1,54 @@
 package ejercicios.ejercicioManiferosPoo;
 
-public class Leon {
+import ejercicios.ejercicioManiferosPoo.tiposManiferos.Felino;
+
+public class Leon extends Felino{
+
+    private Integer numManada;
+    private Float potenciaRugido;
+
+    public Leon() {
+        super();
+    }
+    public Leon(String habitat, Float altura, Float largo, Float peso, Float nombreCientifico, Float tamanoGarras, int velocidad, Integer numManada, Float potenciaRugido) {
+        super(habitat, altura, largo, peso, nombreCientifico, tamanoGarras, velocidad);
+        this.numManada = numManada;
+        this.potenciaRugido = potenciaRugido;
+    }    
+
+    public Integer getNumManada() {
+        return numManada;
+    }
+    public void setNumManada(Integer numManada) {
+        this.numManada = numManada;
+    }
+    public Float getPotenciaRugido() {
+        return potenciaRugido;
+    }
+    public void setPotenciaRugido(Float potenciaRugido) {
+        this.potenciaRugido = potenciaRugido;
+    }
+
+    @Override
+    public String comer() {
+        return "El Leon de especie, ruge con una potencia de " + getPotenciaRugido() + " en su habitat " + getHabitat() + " come con sus garras de " + getTamanoGarras() + " y come a su presa";
+    }
+
+    @Override
+    public String dormir() {
+        return "El leon duerme con su manada de " + getNumManada() + " en su habitat " + getHabitat();
+    }
+
+    @Override
+    public String correr() {
+        return "El leon correr por su habitat " + getHabitat();
+    }
+
+    @Override
+    public String comunicarse() {
+        return "El leon se comunica con su mandada de " + getNumManada() + " en su habitat " + getHabitat();
+    }
+
+    
 
 }
