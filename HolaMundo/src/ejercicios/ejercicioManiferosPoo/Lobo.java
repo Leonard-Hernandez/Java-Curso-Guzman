aaba5afeae6380718b5a9ecec0ad7276c33bb14b
Leonard <Leonardhernandez02@gmail.com>
2024-06-07 21:20:46 -0500

    terminando la implementacion de los caninos

--- HolaMundo/src/ejercicios/ejercicioManiferosPoo/Lobo.java
+++ HolaMundo/src/ejercicios/ejercicioManiferosPoo/Lobo.java
@@ -1,5 +1,54 @@
 package ejercicios.ejercicioManiferosPoo;
 
-public class Lobo {
+import ejercicios.ejercicioManiferosPoo.tiposManiferos.Canino;
+
+public class Lobo extends Canino{
+
+    private String color;
+    private Float tamanoColmillos;
+
+    public Lobo() {}
+
+    public Lobo(String habitat, Float altura, Float largo, Float peso, Float nombreCientifico, String color, Float tamanoColmillos) {
+        super(habitat, altura, largo, peso, nombreCientifico, color, tamanoColmillos);
+        this.color = color;
+        this.tamanoColmillos = tamanoColmillos;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Float getTamanoColmillos() {
+        return tamanoColmillos;
+    }
+
+    public void setTamanoColmillos(Float tamanoColmillos) {
+        this.tamanoColmillos = tamanoColmillos;
+    }
+
+    @Override
+    public String comer() {
+        return "El lobo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " come ratones con sus colmillos de " + getTamanoColmillos() + " y come a su presa";
+    }
+
+    @Override
+    public String dormir() {
+        return "El lobo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " duerme";
+    }
+
+    @Override
+    public String correr() {
+        return "El lobo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " corre";
+    }
+
+    @Override
+    public String comunicarse() {
+        return "El lobo de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " comunica";
+    }
 
 }
--- HolaMundo/src/ejercicios/ejercicioManiferosPoo/Perro.java
+++ HolaMundo/src/ejercicios/ejercicioManiferosPoo/Perro.java
@@ -1,5 +1,44 @@
 package ejercicios.ejercicioManiferosPoo;
 
-public class Perro {
+import ejercicios.ejercicioManiferosPoo.tiposManiferos.Canino;
+
+public class Perro extends Canino{
+
+    private Integer fuerzaMordida;
+
+    public Perro() {}
+
+    public Perro(String habitat, Float altura, Float largo, Float peso, Float nombreCientifico, String color, Float tamanoColmillos, Integer fuerzaMordida) {
+        super(habitat, altura, largo, peso, nombreCientifico, color, tamanoColmillos);
+        this.fuerzaMordida = fuerzaMordida;
+    }
+
+    public Integer getFuerzaMordida() {
+        return this.fuerzaMordida;
+    }
+
+    public void setFuerzaMordida(Integer fuerzaMordida) {
+        this.fuerzaMordida = fuerzaMordida;
+    }
+
+    @Override
+    public String comer() {
+        return "El perro de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " come ratones con sus colmillos de " + getTamanoColmillos() + " y come a su presa";
+    }
+
+    @Override
+    public String dormir() {
+        return "El perro de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " duerme";
+    }
+
+    @Override
+    public String correr() {
+        return "El perro de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " corre";
+    }
+
+    @Override
+    public String comunicarse() {
+        return "El perro de especie " + getNombreCientifico() + " en su habitat " + getHabitat() + " comunica";
+    }
 
 }
