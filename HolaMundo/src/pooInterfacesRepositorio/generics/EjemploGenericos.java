03ebf3ae751aae1c61e1b7ba25bde3631986e8a4
Leonard <Leonardhernandez02@gmail.com>
2024-06-14 18:51:36 -0500

    usando genericos

---
+++ HolaMundo/src/pooInterfacesRepositorio/generics/EjemploGenericos.java
@@ -0,0 +1,43 @@
+package pooInterfacesRepositorio.generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import pooInterfacesRepositorio.modelo.Cliente;
+
+public class EjemploGenericos {
+
+    public static void main(String[] args) {
+
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente("Leonard", "Hernandez"));
+
+        Cliente leonard = (Cliente) clientes.get(0);
+
+        Cliente[] clientesArreglo = { new Cliente("Leonard", "Hernandez"), new Cliente("Lubraska", "Ferreira") };
+
+        Integer[] enteros = {1, 2, 3, 4, 5};
+
+        List<Cliente> clientes2 = fromArraytoList(clientesArreglo);
+        List<Integer> enteros2 = fromArraytoList(enteros);
+
+        clientes2.forEach(System.out::println);
+        enteros2.forEach(System.out::println);
+
+        List<String> nombres = fromArraytoList(new String[]{"Leonard", "Lubraska",
+         "Jose", "Luis", "Maria"}, enteros2);
+
+    }
+
+    public static <T> List<T> fromArraytoList(T[] c) {
+        return Arrays.asList(c);
+    }
+
+    public static <T,G> List<T> fromArraytoList(T[] c, G[] x) {
+        for(G elemento: x){
+            System.out.println(elemento);
+        }
+        return Arrays.asList(c);
+    }
+}
