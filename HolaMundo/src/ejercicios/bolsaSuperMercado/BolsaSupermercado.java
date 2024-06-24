5c3af4efae03751b0ef9c4a9ad8587cb0a216655
Leonard <Leonardhernandez02@gmail.com>
2024-06-23 19:24:27 -0500

    implementando la clase bolsa super mercado

--- HolaMundo/src/ejercicios/bolsaSuperMercado/BolsaSupermercado.java
+++ HolaMundo/src/ejercicios/bolsaSuperMercado/BolsaSupermercado.java
@@ -1,5 +1,22 @@
 package ejercicios.bolsaSuperMercado;
 
-public class BolsaSupermercado {
+import java.util.List;
+
+public class BolsaSupermercado<T> {
+
+    private List<T> contenido;
+    private int indice;
+    public static final int MAX_PRODUCTOS = 5;
+
+    public void addProducto(T t) {
+        if (indice < MAX_PRODUCTOS) {
+            contenido.add(t);
+            indice++;
+        }
+    }
+
+    public List<T> getProductos() {
+        return this.contenido;
+    }
 
 }
