6522f35dbaf2e4791ec5ff60b207668b7586555a
Leonard <Leonardhernandez02@gmail.com>
2024-07-01 16:07:30 -0500

    ejemplo tree set comparable y comparator

---
+++ HolaMundo/src/colecciones/set/EjemploTreeSetComparable.java
@@ -0,0 +1,25 @@
+package colecciones.set;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import colecciones.modelo.Alumno;
+
+public class EjemploTreeSetComparable {
+
+    public static void main(String[] args) {
+        
+        Set<Alumno> sa = new TreeSet<>(Comparator.reverseOrder());
+        sa.add(new Alumno("Lubraska ",10));
+        sa.add(new Alumno("Leonard", 7));
+        sa.add(new Alumno("Rodolfo", 4));
+        sa.add(new Alumno("Santiago", 8));
+        sa.add(new Alumno("Jose", 1));
+
+        System.out.println(sa);
+
+
+    }
+
+}
