5a1be743d528373519237b8c99442e123054e896
Leonard <Leonardhernandez02@gmail.com>
2024-07-02 19:48:40 -0500

    hash set unicidad

--- HolaMundo/src/colecciones/set/EjemploHashSetUnicidad.java
+++ HolaMundo/src/colecciones/set/EjemploHashSetUnicidad.java
@@ -1,24 +1,23 @@
 package colecciones.set;
 
-import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 import colecciones.modelo.Alumno;
 
-public class EjemploTreeSetComparable {
+public class EjemploHashSetUnicidad {
 
     public static void main(String[] args) {
-        
-        Set<Alumno> sa = new TreeSet<>((a,b) -> b.getNombre().compareTo(b.getNombre()));
-        sa.add(new Alumno("Lubraska ",10));
+
+        Set<Alumno> sa = new HashSet<>();
+        sa.add(new Alumno("Lubraska ", 10));
         sa.add(new Alumno("Leonard", 7));
         sa.add(new Alumno("Rodolfo", 4));
         sa.add(new Alumno("Santiago", 8));
         sa.add(new Alumno("Jose", 1));
+        sa.add(new Alumno("Jose", 12));
 
         System.out.println(sa);
-
 
     }
 
