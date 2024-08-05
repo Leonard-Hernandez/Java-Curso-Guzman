a29fce7dfc92046045769e376de93e1b4ecc4ac6
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-08-05 16:10:07 -0500

    maps anidados

--- HolaMundo/src/colecciones/map/EjemploHashMap.java
+++ HolaMundo/src/colecciones/map/EjemploHashMap.java
@@ -8,24 +8,41 @@ import java.util.Set;
 public class EjemploHashMap {
     public static void main(String[] args) {
         
-        Map<String, String> persona = new HashMap<>();
+        Map<String, Object> persona = new HashMap<>();
         System.out.println("Tiene elementos " + !persona.isEmpty());
 
         persona.put("nombre", "John");
         persona.put("apellido", "Doe");
         persona.put("apellidoPaterno", "Roe");
         persona.put("email", "john.doe@carvajal.com");
-        persona.put("edad", "30");
+        persona.put("edad", 30);
+
+        Map<String, String> direccion = new HashMap<>();
+        direccion.put("pais", "Venezuela");
+        direccion.put("estado", "Tachira");
+        direccion.put("ciudad", "San Cristobal");
+        direccion.put("calle", "calle 13");
+        direccion.put("numero", "25d");
+
+        persona.put("direccion", direccion);
 
         System.out.println("persona = " + persona);
 
-        String nombre = persona.get("nombre");
+        String nombre = (String) persona.get("nombre");
         System.out.println(nombre);
-        String apellido = persona.get("apellido");
+        String apellido = (String) persona.get("apellido");
         System.out.println(apellido);
 
+        Map<String, String> direccionPersona = (Map<String, String>) persona.get("direccion");
+        String pais = direccionPersona.get("pais");
+        String ciudad = direccionPersona.get("ciudad");
+        String barrio = direccionPersona.getOrDefault("barrio", "Centro");
+        System.out.println("el pais de " + nombre + " es: " + pais);
+        System.out.println("la ciudad de " + nombre + " es: " + ciudad);
+        System.out.println("el barrio de " + nombre + " es: " + barrio);
+
         // devuelve el valor eliminado
-        String valor = persona.remove("apellidoPaterno");
+        String valor = (String) persona.remove("apellidoPaterno");
         // devuelve un boolean dependiendo de si se borro
         boolean eliminado = persona.remove("apellido", "Doe");
 
@@ -35,9 +52,9 @@ public class EjemploHashMap {
         boolean contiene = persona.containsValue("30");
         System.out.println("contiene la el valor 30 = " + contiene);
 
-        Collection<String> valores = persona.values();
+        Collection<Object> valores =  persona.values();
 
-        for(String v: valores){
+        for(Object v: valores){
             System.out.println("Valor = " + v);
         }
 
@@ -47,12 +64,12 @@ public class EjemploHashMap {
         }
 
         System.out.println("=============================== entrySet");
-        for(Map.Entry<String, String> par: persona.entrySet()){
+        for(Map.Entry<String, Object> par: persona.entrySet()){
             System.out.println(par.getKey() + " => " + par.getValue());
         }
         System.out.println("=============================== keySet");
         for(String llave: persona.keySet()){
-            String v = persona.get(llave);
+            Object v =  persona.get(llave);
             System.out.println(llave + " => " + v);
         }
 
