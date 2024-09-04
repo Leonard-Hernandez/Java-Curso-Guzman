11737e0f9608bc003d0c2567f685e9d5e1b298f9
Leonard <Leonardhernandez02@gmail.com>
2024-09-04 16:30:08 -0500

    join vs sleep

--- HolaMundo/src/thread/hilos/ejemplo/EjemploInterfazRunnableJava8.java
+++ HolaMundo/src/thread/hilos/ejemplo/EjemploInterfazRunnableJava8.java
@@ -2,8 +2,9 @@ package thread.hilos.ejemplo;
 
 public class EjemploInterfazRunnableJava8 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Thread main = Thread.currentThread();
         Runnable viaje = () -> {
                 for (int i = 0; i < 10; i++) {
                     System.out.println(i + " - " + Thread.currentThread().getName());
@@ -16,12 +17,27 @@ public class EjemploInterfazRunnableJava8 {
                 }
         
                 System.out.println("finalmente me voy de viaje " + Thread.currentThread().getName());
+                System.out.println(main.getState());
             };
 
-        new Thread(viaje, "cucuta").start();
-        new Thread(viaje, "cali").start();
-        new Thread(viaje, "bogota").start();
-        new Thread(viaje, "medellin").start();
+        Thread v1 = new Thread(viaje, "cucuta");
+        Thread v2 = new Thread(viaje, "cali");
+        Thread v3 = new Thread(viaje, "bogota");
+        Thread v4 = new Thread(viaje, "medellin");
+        
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
+
+        v1.join();
+        v2.join();
+        v3.join();
+        v4.join();
+        
+        //Thread.sleep(1000);
+        System.out.println("Continuando la ejecucion del metodo main: " + main.getName());
+        System.out.println(main.getState());
 
     }
 
