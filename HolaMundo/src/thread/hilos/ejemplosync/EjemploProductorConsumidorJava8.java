20f0dfc219005c0089e63eb3eb5b43b9fe0dd80f
Leonard <Leonardhernandez02@gmail.com>
2024-09-08 18:03:00 -0500

    ejemplo consumidor productor con expresiones lambda

---
+++ HolaMundo/src/thread/hilos/ejemplosync/EjemploProductorConsumidorJava8.java
@@ -0,0 +1,29 @@
+package thread.hilos.ejemplosync;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class EjemploProductorConsumidorJava8 {
+
+    public static void main(String[] args) {
+        Panaderia p = new Panaderia();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                p.hornear("Pan n: " + i);
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(500, 2000));
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10;i++) {
+                p.consumir();
+            }
+        }).start();
+    }
+
+}
--- HolaMundo/src/thread/hilos/ejemplosync/runnable/Consumidor.java
+++ HolaMundo/src/thread/hilos/ejemplosync/runnable/Consumidor.java
@@ -13,7 +13,7 @@ public class Consumidor implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 10;i++) {
-            System.out.println("Consumidor consume: " + panaderia.consumir());
+            panaderia.consumir();
         }
     }
 
