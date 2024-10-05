7bbd0e7601d45866876e4b0eb27651a21c136943
Leonard <Leonardhernandez02@gmail.com>
2024-10-04 21:00:13 -0500

    Mapeando la Entidad Usuario con un Stream

--- HolaMundo/src/apiStream/ejemplo/EjemploStreamMap.java
+++ HolaMundo/src/apiStream/ejemplo/EjemploStreamMap.java
@@ -1,22 +1,27 @@
 package apiStream.ejemplo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import apiStream.ejemplo.models.Usuario;
+
 public class EjemploStreamMap {
     public static void main(String[] args) {
 
-        Stream<String> nombres = Stream
-                .of("leonard", "Lubraska", "Andres", "Jose", "Luis")
-                .map(String::toUpperCase)
-                .peek(System.out::println)
-                .map(String::toLowerCase);
-        nombres.forEach(System.out::println);
+        Stream<Usuario> nombres = Stream
+                .of("leonard hernandez", "Lubraska ferreira", "Andres guzman", "Jose jose", "Luis miguel")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .peek(u -> System.out.println(u.getNombre()))
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                });
+        //nombres.forEach(System.out::println);
 
-        List<String> lista = nombres.collect(Collectors.toList());
+        List<Usuario> lista = nombres.collect(Collectors.toList());
+        lista.forEach(u -> System.out.println(u.getApellido()));
 
     }
 }
