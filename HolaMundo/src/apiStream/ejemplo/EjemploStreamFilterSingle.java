84ac7f5a3e4d68f51ff9c55262623e271f0cc726
leonard hernandez <leonardhernandez02@gmail.com>
2024-10-08 17:47:05 -0500

    Usos api optional

--- HolaMundo/src/apiStream/ejemplo/EjemploStreamFilterSingle.java
+++ HolaMundo/src/apiStream/ejemplo/EjemploStreamFilterSingle.java
@@ -12,12 +12,18 @@ public class EjemploStreamFilterSingle {
                 .of("leonard hernandez", "Lubraska ferreira", "Andres guzman", "Jose jose", "Luis miguel",
                         "Pepe Garcia", "Pepe Jose")
                 .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
-                .filter(u -> u.getNombre().equals("Pepe"))
+                .filter(u -> u.getNombre().equals("Pepex"))
                 .peek(System.out::println);
         // nombres.forEach(System.out::println);
 
         Optional<Usuario> usuario = nombres.findFirst();
-        System.out.println(usuario.get().getApellido());
-
+        //System.out.println(usuario.orElse(new Usuario("Jhon", "Doe")).getApellido());
+        //System.out.println(usuario.orElseGet(() -> new Usuario("Jhon", "Doe")).getApellido());
+        if (usuario.isPresent()){
+            System.out.println(usuario.get().getApellido());
+        }else {
+            System.out.println("no se encontro el objeto");
+        }
+        //System.out.println(usuario.orElseThrow());
     }
 }
